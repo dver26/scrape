cd28f8dfbfb749c02a1ef7dabaf82cc836f13cbc
dver26 <fercorvictor@gmail.com>
2023-01-20 19:20:50 +0100

    deleted commented code

--- index.js
+++ index.js
@@ -36,34 +36,6 @@ const shops = [
       return notAvailableIcon.length === 0
     }
   }
-  /* // disabled for now because it's not working properly
-  {
-    vendor: 'MediaMarkt',
-    hasSchema: true,
-    url: 'https://www.mediamarkt.es/es/product/_consola-microsoft-xbox-series-s-512-gb-ssd-blanco-1487616.html',
-    // furncion para encontrar si está disponible en la web
-    checkStock: async ({ page }) => {
-      const content = await page.textContent('[data-test="pdp-product-not-available"]')
-      return content.includes('no está disponible') === false
-    }
-  },
-  // disabled for now because it's not working properly
-  {
-    vendor: 'Amazon',
-    hasSchema: false,
-    url: 'https://www.amazon.es/Microsoft-Xbox-Series-X-Standard/dp/B08JDSW1ZW/ref=asc_df_B08JDSW1ZW/?tag=googshopes-21&linkCode=df0&hvadid=593142465768&hvpos=&hvnetw=g&hvrand=15485825285777940441&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=20294&hvtargid=pla-1043815071114&psc=1',
-    // furncion para encontrar si está disponible en la web
-    checkStock: async ({ page }) => {
-      const addToCartButton = await page.textContent('[id="add-to-cart-button"]')
-      return addToCartButton.includes('cesta') === true
-    }
-  }, */
-  /* {
-    vendor: 'El Corte Inglés',
-    hasSchema: false
-    url: 'https://www.elcorteingles.es/videojuegos/A45506578-xbox-series-s-gilded-hunter-bundle/',
-    checkStock: async ({ page }) => {}
-  } */
 ]
 
 // funcion principal del proyecto
